5277fbc08dba2f896957eab51e4984876e99e117
Enoch Park <44038883+enochpark89@users.noreply.github.com>
2022-07-22 19:23:12 -0700

    Going to push to Heroku and Git

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ def home():
     
     return render_template("search.html")
 
-@app.route('/<keyword>')
-def found(keyword):
-    jobs = get_job(keyword)
-    return render_template("result.html", jobs=jobs)
+# @app.route('/<keyword>')
+# def found(keyword):
+#     jobs = get_job(keyword)
+#     return render_template("result.html", jobs=jobs)
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
